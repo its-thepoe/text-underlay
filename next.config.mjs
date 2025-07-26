2a620a84d53d7446c5c774d6cbb409e8cef88c4c
Oladipupo Ayoola <oladipupoayoola01@gmail.com>
2025-07-26 21:33:42 +0100

    Fixes next structure issue

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
               protocol: 'https',
               hostname: 'assets.dub.co',
             },
+            {
+              protocol: 'https',
+              hostname: 'picsum.photos',
+            },
           ],
     },
 
