b3c6bd35e4fc74e3f2f1e5e649dc926d5159bdfe
Oladipupo Ayoola <oladipupoayoola01@gmail.com>
2025-12-10 09:23:47 +0100

    Fixes lint

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -73,87 +73,98 @@ function ShineBorder({
 }
 
 // Canvas animation code
-function n(e: any) {
-  this.init(e || {});
-}
-n.prototype = {
-  init: function (e: any) {
+class Oscillator {
+  phase: number;
+  offset: number;
+  frequency: number;
+  amplitude: number;
+
+  constructor(e: any = {}) {
     this.phase = e.phase || 0;
     this.offset = e.offset || 0;
     this.frequency = e.frequency || 0.001;
     this.amplitude = e.amplitude || 1;
-  },
-  update: function () {
-    this.phase += this.frequency;
-    return (e = this.offset + Math.sin(this.phase) * this.amplitude);
-  },
-  value: function () {
-    return e;
-  },
-};
+  }
 
-function Line(e: any) {
-  this.init(e || {});
+  update() {
+    this.phase += this.frequency;
+    e = this.offset + Math.sin(this.phase) * this.amplitude;
+    return e;
+  }
+
+  value() {
+    return e;
+  }
 }
 
-Line.prototype = {
-  init: function (e: any) {
+class Line {
+  spring: number;
+  friction: number;
+  nodes: Node[];
+
+  constructor(e: any = {}) {
     this.spring = e.spring + 0.1 * Math.random() - 0.05;
     this.friction = E.friction + 0.01 * Math.random() - 0.005;
     this.nodes = [];
-    for (var t, n = 0; n < E.size; n++) {
-      t = new Node();
+    for (let n = 0; n < E.size; n++) {
+      const t = new Node();
       t.x = pos.x;
       t.y = pos.y;
       this.nodes.push(t);
     }
-  },
-  update: function () {
-    let e = this.spring,
-      t = this.nodes[0];
-    t.vx += (pos.x - t.x) * e;
-    t.vy += (pos.y - t.y) * e;
-    for (var n, i = 0, a = this.nodes.length; i < a; i++)
-      (t = this.nodes[i]),
-        0 < i &&
-          ((n = this.nodes[i - 1]),
-          (t.vx += (n.x - t.x) * e),
-          (t.vy += (n.y - t.y) * e),
-          (t.vx += n.vx * E.dampening),
-          (t.vy += n.vy * E.dampening)),
-        (t.vx *= this.friction),
-        (t.vy *= this.friction),
-        (t.x += t.vx),
-        (t.y += t.vy),
-        (e *= E.tension);
-  },
-  draw: function () {
-    let e,
-      t,
-      n = this.nodes[0].x,
-      i = this.nodes[0].y;
-    ctx.beginPath();
-    ctx.moveTo(n, i);
-    for (var a = 1, o = this.nodes.length - 2; a < o; a++) {
-      e = this.nodes[a];
-      t = this.nodes[a + 1];
-      n = 0.5 * (e.x + t.x);
-      i = 0.5 * (e.y + t.y);
-      ctx.quadraticCurveTo(e.x, e.y, n, i);
+  }
+
+  update() {
+    let spring = this.spring;
+    let t = this.nodes[0];
+    t.vx += (pos.x - t.x) * spring;
+    t.vy += (pos.y - t.y) * spring;
+    for (let i = 0, a = this.nodes.length; i < a; i++) {
+      t = this.nodes[i];
+      if (i > 0) {
+        const n = this.nodes[i - 1];
+        t.vx += (n.x - t.x) * spring;
+        t.vy += (n.y - t.y) * spring;
+        t.vx += n.vx * E.dampening;
+        t.vy += n.vy * E.dampening;
+      }
+      t.vx *= this.friction;
+      t.vy *= this.friction;
+      t.x += t.vx;
+      t.y += t.vy;
+      spring *= E.tension;
     }
-    e = this.nodes[a];
-    t = this.nodes[a + 1];
-    ctx.quadraticCurveTo(e.x, e.y, t.x, t.y);
+  }
+
+  draw() {
+    let nodeA,
+      nodeB,
+      x = this.nodes[0].x,
+      y = this.nodes[0].y;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    let a = 1;
+    const o = this.nodes.length - 2;
+    for (; a < o; a++) {
+      nodeA = this.nodes[a];
+      nodeB = this.nodes[a + 1];
+      x = 0.5 * (nodeA.x + nodeB.x);
+      y = 0.5 * (nodeA.y + nodeB.y);
+      ctx.quadraticCurveTo(nodeA.x, nodeA.y, x, y);
+    }
+    nodeA = this.nodes[a];
+    nodeB = this.nodes[a + 1];
+    ctx.quadraticCurveTo(nodeA.x, nodeA.y, nodeB.x, nodeB.y);
     ctx.stroke();
     ctx.closePath();
-  },
-};
+  }
+}
 
 function onMousemove(e: any) {
   function o() {
     lines = [];
     for (let e = 0; e < E.trails; e++)
-      lines.push(new Line({ spring: 0.45 + (e / E.trails) * 0.025 }));
+      lines.push(new Line({ spring: 0.45 + (e / E.trails) * 0.025 }) as any);
   }
   function c(e: any) {
     e.touches
@@ -211,20 +222,21 @@ var ctx: any,
     tension: 0.99,
   };
 
-function Node() {
-  this.x = 0;
-  this.y = 0;
-  this.vy = 0;
-  this.vx = 0;
+class Node {
+  x: number = 0;
+  y: number = 0;
+  vx: number = 0;
+  vy: number = 0;
 }
 
 const renderCanvas = function () {
-  ctx = document.getElementById("canvas")?.getContext("2d");
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
+  ctx = canvas?.getContext("2d");
   if (!ctx) return;
   
   ctx.running = true;
   ctx.frame = 1;
-  f = new n({
+  f = new Oscillator({
     phase: Math.random() * 2 * Math.PI,
     amplitude: 85,
     frequency: 0.0015,
